8a825a032d781cb85ac340116451f1ff4003b2fd
Mohamed Mostafa <mohamed.shaban@microengineering.ca>
2026-07-09 12:25:49 +0300

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:5000")
+    fetch("test-frontend-bakckend-vercel-fq3jlv9ai-amg16.vercel.app")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
