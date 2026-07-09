b8e3088b201b918c78da72389d324a62d6d06894
Mohamed Mostafa <mohamed.shaban@microengineering.ca>
2026-07-09 16:58:48 +0300

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("https://test-frontend-bakckend-vercel-zgua-rncbohcyn-amg16.vercel.app/")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+    axios
+      .get("https://test-frontend-bakckend-vercel-zgua-rncbohcyn-amg16.vercel.app/")
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Axios Error:", error);
+
+        if (error.response) {
+          console.log("Status:", error.response.status);
+          console.log("Data:", error.response.data);
+        } else if (error.request) {
+          console.log("No response received");
+        } else {
+          console.log(error.message);
+        }
+      });
   }, []);
 
   return (
