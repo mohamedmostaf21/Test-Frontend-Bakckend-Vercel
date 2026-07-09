815bf7f7a605e1602925916c481c430f0c8f618b
Mohamed Mostafa <mohamed.shaban@microengineering.ca>
2026-07-09 12:43:09 +0300

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
+app.use(cors({
+  origin: "https://test-frontend-bakckend-vercel-c7a2.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "Hello from Backend"
-    });
+  res.json({ message: "Hello Backend" });
 });
 
-app.get("/health", (req, res) => {
-    res.json({
-        status: "OK"
-    });
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Running on ${PORT}`);
-});
+module.exports = app; // For Vercel
